35fbe8bc2498fd6c268bc2b39679ff807eeaa587
Aastha <awasthiaastha1999@gmail.com>
2022-04-22 13:29:22 +0530

    CSV file included

--- main.py
+++ main.py
@@ -1,69 +1,6 @@
-"""# importing required modules
-import PyPDF2
 
-# creating a pdf file object
-pdfFileObj = open('File.pdf', 'rb')
-
-# creating a pdf reader object
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-
-# printing number of pages in pdf file
-print(pdfReader.numPages)
-
-# creating a page object
-pageObj = pdfReader.getPage(0)
-
-# extracting text from page
-print(pageObj.extractText())
-
-# closing the pdf file object
-pdfFileObj.close()"""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import csv
+from msilib.schema import File
 import re
 import pdfplumber
 import pandas as pd
@@ -79,13 +16,13 @@ def numbify(num):
     return int(num.replace('$', '').replace(',',''))
 
 def page_to_df(lines , last_pos , name_info =('','','','','','','')):
-    jrnl, journal_name, phone ,mobile,email, Office_Address , Residence_Address = name_info
+    jrnl, person_name, phone ,mobile,email, Office_Address , Residence_Address = name_info
     rows = []
 
     for idx , line in enumerate(lines.split('\n')):
         if line.startswith('NAME'):
             jrnl = jrnl_re.search(line)
-            journal_name = jrnl.group(1)
+            person_name = jrnl.group(1)
             phone = jrnl.group(2)
             mobile = jrnl.group(3)
             email = jrnl.group(4)
@@ -93,7 +30,7 @@ def page_to_df(lines , last_pos , name_info =('','','','','','','')):
             Residence_Address = jrnl.group(6)
 
         df = pd.DataFrame(rows)
-    return df , (jrnl,journal_name,phone,mobile , email,Office_Address,Residence_Address)
+    return df , (jrnl,person_name,phone,mobile , email,Office_Address,Residence_Address)
 
 df = pd.DataFrame()
 name_info = ('','','','','','' , '')
@@ -105,10 +42,9 @@ with pdfplumber.open("File.pdf") as pdf:
         last_pos = {rows_dict.get(word['bottom']): word['x1'] for word in words}
         new_df , name_info = page_to_df(lines , last_pos , name_info )
         df = pd.concat([df,new_df]).reset_index(drop=True)
-
+        
+        
 with pdfplumber.open("File.pdf") as pdf:
     lines = pdf.pages[0].extract_text(x_tolerance=3, y_tolerance=4)
     print(lines)
-
-
 
